c7f23195ef58d894081688c94166e0c925b07f59
deeepika-Apical <deeepikavalmiki@apicalsoftsolution.com>
2026-02-26 23:13:24 +0530

    contactusdetailes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import NurseRegistrationForm from "./Components/Registration/NurseRegistrationForm";
 import PatientRegistrationForm from "./Components/Registration/PatientRegistrationForm";
 import PatientList from "./Components/Registration/PatientList";
+import ContactUs from "./Components/ContactAccess/ContactUs";
 import "./Components/Registration/NurseForm.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/register" element={<NurseRegistrationForm />} />
         <Route path="/patient-register" element={<PatientRegistrationForm />} />
+        <Route path="/patients" element={<PatientList />} /> 
+        <Route path="/ContactUs" element={<ContactUs/>} />
       </Routes>
     </BrowserRouter>
     // <div className="App">
